567109c9d9cf2258bba1620f193d87336378ebc8
Gerardo Munguia <gerardomunguiahoyo@gmail.com>
2018-06-03 12:40:40 +0200

    Move connection config to input variables

--- test.js
+++ test.js
@@ -1,5 +1,9 @@
 "use strict";
 
 const testIntegration = require("../any-queue-test/index.js");
-const { connect, initialize, refresh, disconnect } = require(".");
+const { connect, initialize, refresh, disconnect } = require(".")({
+  database: "any-queue-test",
+  url: "mongodb://localhost:27017"
+});
+
 testIntegration({ name: "mongo", connect, initialize, refresh, disconnect });
